3bf52e83000236998c9ec36c21e00e0dd8468349
Daniel Idowu <74189249+dharniel45@users.noreply.github.com>
2023-09-07 15:05:37 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,36 +4,29 @@ import client from "./apollo-client.js";
 import Home from './pages/index.js';
 import React, { useState } from 'react';
 
-
 function App(Component, pageProps) {
   const [dataLoaded, setDataLoaded] = useState(false);
 
   const handleButtonClick = () => {
-    setDataLoaded(true);
+    setDataLoaded(false);
   };
-
-
 
   return (
     <div className="App">
-      
       <div className="App-header">
         <h1 className='starter'>Keep track of all your NFT projects</h1>
-        
+  
         <div className="query">
-        <button onClick={handleButtonClick}>Fetch Latest Data</button>
+          <button onClick={handleButtonClick}>Fetch Latest Data</button>
         </div>
+      </div>
       
-      
+      <div className='data'>
+        <ApolloProvider client={client}>
+          <Home {...pageProps} dataLoaded={dataLoaded} setDataLoaded={setDataLoaded} />
+        </ApolloProvider>
+      </div>
     </div>
-    <div className='data'>
-      <ApolloProvider client={client}>
-      <Home {...pageProps} dataLoaded={dataLoaded} /> 
-    </ApolloProvider>
-    </div>
-    
-    </div>
-    
   );
 }
 
